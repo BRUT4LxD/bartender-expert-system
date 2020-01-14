a7ceb8d468ce9dd742e7c24ebdb1ccecc4b3769e
Pawel Tarsala (PLT) <PLT@kmd.dk>
2020-01-14 22:50:48 +0100

    Add visual studio project

---
+++ BartenderExpertSystem/BartenderExpertSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BartenderExpertSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
